e1dce6cfe9694fd366f107eeaf467e08536cdbf2
Darnyex <54082504+Darnyex@users.noreply.github.com>
2019-09-07 18:08:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 const fs = require("fs");
 
 
-const token = 'NjEyMDkyMzY0NjU1Mjk2NTM4.XVdVow.RjAbjUk0pZ7gQQD4W1zNQXl5tZs';
+const token = 'NjEyMDkyMzY0NjU1Mjk2NTM4.XXQ4Tg.fXxMzif9QPSzWUCYualVuE9Kp9Q';
 
 const prefix = '>';
 let xp = require("./xp.json");
